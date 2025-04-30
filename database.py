d6c9217206af16bd4d07298995c43b49390333b6
BinhPhuc <pham.phuc.binh.271106@gmail.com>
2025-04-30 22:49:12 +0700

    create db

---
+++ database.py
@@ -0,0 +1,8 @@
+from sqlmodel import SQLModel, create_engine
+import pymysql
+DATABASE_URL = "mysql+pymysql://root:binh2006@localhost:3306/heroapplication"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
