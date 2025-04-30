4fdb3405f9fb1dc790f8bc472c04819eddbfad59
BinhPhuc <pham.phuc.binh.271106@gmail.com>
2025-05-01 01:41:02 +0700

    get session function to reuse

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import SQLModel, create_engine, Session
 DATABASE_URL = "mysql+pymysql://root:binh2006@localhost:3306/heroapplication"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
 def init_db():
     SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
