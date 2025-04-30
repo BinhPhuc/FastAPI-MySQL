ee04636a94116f04bb747cf6f5319cccf770cddb
BinhPhuc <pham.phuc.binh.271106@gmail.com>
2025-05-01 01:40:29 +0700

    more model (same as DTO)

--- models.py
+++ models.py
@@ -1,8 +1,18 @@
 from sqlmodel import Field, SQLModel
-from typing import Optional
+from typing import Annotated
 
 class Hero(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    secret_name: str
+    age: int | None = None
+
+class HeroCreate(SQLModel): # Same as HeroDTO in Java Spring Boot
+    name: Annotated[str, Field(..., title="Hero name", max_length=100)]
+    secret_name: Annotated[str, Field(..., title="Secret name", max_length=100)]
+    age: Annotated[int | None, Field(default=None, title="Hero age")]
+
+class HeroResponse(SQLModel):
     name: str
     secret_name: str
     age: int | None = None
